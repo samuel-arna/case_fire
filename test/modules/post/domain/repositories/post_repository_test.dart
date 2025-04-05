b752d1a4aa9548d8230be32b90370e105dac71cb
Samuel Arná <samuka.arna@gmail.com>
2025-04-05 19:12:15 -0300

    fix: post's test

--- test/modules/post/domain/repositories/post_repository_test.dart
+++ test/modules/post/domain/repositories/post_repository_test.dart
@@ -4,20 +4,24 @@ import 'package:case_fire/modules/post/domain/repositories/post_repository.dart'
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Deve pegar os posts', () async {
-    var datasource = PostPlaceholderDatasource();
-    var repository = PostRepositoryImpl(datasource);
-    List<PostModel> listPost = await repository.getPosts();
-    expect(listPost.isNotEmpty, true);
-  });
-  test('Os posts devem conter userId, id, title, body', () async {
-    var datasource = PostPlaceholderDatasource();
-    var repository = PostRepositoryImpl(datasource);
-    List<PostModel> listPost = await repository.getPosts();
-    PostModel post = listPost.first;
-    expect(post.userId, isNotNull);
-    expect(post.id, isNotNull);
-    expect(post.title, isNotNull);
-    expect(post.body, isNotNull);
+  group('Testes Post Repository', () {
+    late PostPlaceholderDatasource datasource;
+    late PostRepositoryImpl repository;
+    setUp(() {
+      datasource = PostPlaceholderDatasource();
+      repository = PostRepositoryImpl(datasource);
+    });
+    test('Deve pegar os posts', () async {
+      List<PostModel> listPost = await repository.getPosts();
+      expect(listPost.isNotEmpty, true);
+    });
+    test('Os posts devem conter userId, id, title, body', () async {
+      List<PostModel> listPost = await repository.getPosts();
+      PostModel post = listPost.first;
+      expect(post.userId, isNotNull);
+      expect(post.id, isNotNull);
+      expect(post.title, isNotNull);
+      expect(post.body, isNotNull);
+    });
   });
 }
