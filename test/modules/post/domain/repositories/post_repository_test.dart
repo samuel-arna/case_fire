9ce472843199446cd2df1fb8a561be3040d2a891
Samuel Arná <samuka.arna@gmail.com>
2025-04-05 19:58:31 -0300

    feat: include mocktail

--- test/modules/post/domain/repositories/post_repository_test.dart
+++ test/modules/post/domain/repositories/post_repository_test.dart
@@ -2,26 +2,59 @@ import 'package:case_fire/modules/post/data/datasources/post_placeholder_datasou
 import 'package:case_fire/modules/post/data/models/post_model.dart';
 import 'package:case_fire/modules/post/domain/repositories/post_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockPostPlaceholderDatasource extends Mock
+    implements PostPlaceholderDatasource {}
 
 void main() {
   group('Testes Post Repository', () {
-    late PostPlaceholderDatasource datasource;
+    late MockPostPlaceholderDatasource mockDatasource;
     late PostRepositoryImpl repository;
+
     setUp(() {
-      datasource = PostPlaceholderDatasource();
-      repository = PostRepositoryImpl(datasource);
+      mockDatasource = MockPostPlaceholderDatasource();
+      repository = PostRepositoryImpl(mockDatasource);
     });
+
     test('Deve pegar os posts', () async {
+      final mockPosts = [
+        {'userId': 1, 'id': 1, 'title': 'Title 1', 'body': 'Body 1'},
+        {'userId': 2, 'id': 2, 'title': 'Title 2', 'body': 'Body 2'},
+        {'userId': 3, 'id': 3, 'title': 'Title 3', 'body': 'Body 3'},
+      ];
+
+      when(() => mockDatasource.getPosts()).thenAnswer((_) async => mockPosts);
+
       List<PostModel> listPost = await repository.getPosts();
-      expect(listPost.isNotEmpty, true);
+
+      expect(listPost, isNotEmpty);
+      expect(listPost, isA<List<PostModel>>());
+      verify(() => mockDatasource.getPosts()).called(1);
     });
-    test('Os posts devem conter userId, id, title, body', () async {
+
+    test('O post deve conter userId, id, title, body', () async {
+      final mockPosts = [
+        {'userId': 1, 'id': 1, 'title': 'Title 1', 'body': 'Body 1'},
+      ];
+
+      when(() => mockDatasource.getPosts()).thenAnswer((_) async => mockPosts);
+
       List<PostModel> listPost = await repository.getPosts();
       PostModel post = listPost.first;
+
+      expect(listPost, isNotEmpty);
       expect(post.userId, isNotNull);
+      expect(post.userId, isA<int>());
       expect(post.id, isNotNull);
+      expect(post.id, isA<int>());
       expect(post.title, isNotNull);
+      expect(post.title, isA<String>());
+      expect(post.title!.isNotEmpty, true);
       expect(post.body, isNotNull);
+      expect(post.body, isA<String>());
+      expect(post.body!.isNotEmpty, true);
+      verify(() => mockDatasource.getPosts()).called(1);
     });
   });
 }
