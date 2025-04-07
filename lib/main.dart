5f9e08273694893bb427f96b90dc5785066175b5
Samuel Arná <samuka.arna@gmail.com>
2025-04-07 16:40:24 -0300

    feat: include firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:case_fire/core/utils/app_colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:case_fire/firebase_options.dart';
 import 'package:case_fire/modules/post/data/datasources/post_placeholder_datasource.dart';
 import 'package:case_fire/modules/post/domain/repositories/post_repository.dart';
 import 'package:case_fire/modules/post/ui/pages/post_page.dart';
@@ -5,7 +8,9 @@ import 'package:case_fire/modules/post/ui/stores/bloc/post_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
@@ -26,6 +31,15 @@ class MyApp extends StatelessWidget {
         BlocProvider<PostBloc>(create: (context) => PostBloc(_postRepository)),
       ],
       child: MaterialApp(
+        theme: ThemeData(
+          scaffoldBackgroundColor: AppColors.background,
+          appBarTheme: AppBarTheme(
+            backgroundColor: AppColors.cardBackground,
+            surfaceTintColor: AppColors.background,
+            iconTheme: IconThemeData(color: AppColors.textColor),
+            titleTextStyle: TextStyle(color: AppColors.textColor, fontSize: 21),
+          ),
+        ),
         debugShowCheckedModeBanner: false,
         title: 'Case Fire',
         home: const PostPage(),
