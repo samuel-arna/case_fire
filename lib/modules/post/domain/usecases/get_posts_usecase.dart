3d5a505f71a2318d41c4fba5d2fd1747729c14e3
Samuel Arná <samuka.arna@gmail.com>
2025-04-07 23:26:18 -0300

    fix: include either and require dependency

--- lib/modules/post/domain/usecases/get_posts_usecase.dart
+++ lib/modules/post/domain/usecases/get_posts_usecase.dart
@@ -1,19 +1,23 @@
+import 'package:case_fire/core/errors/failure.dart';
+import 'package:case_fire/core/errors/generic_failure.dart';
 import 'package:case_fire/modules/post/domain/entities/post_entity.dart';
 import 'package:case_fire/modules/post/domain/repositories/post_repository.dart';
+import 'package:either_dart/either.dart';
 
 class GetPostsUseCase {
   final PostRepository repository;
 
   GetPostsUseCase(this.repository);
 
-  Future<List<PostEntity>> execute() async {
+  Future<Either<Failure, List<PostEntity>>> execute() async {
     try {
       final posts = await repository.getPosts();
-
-      return posts.map((model) => PostEntity.fromModel(model)).toList();
+      final List<PostEntity> postEntities =
+          posts.map((model) => PostEntity.fromModel(model)).toList();
+      return Right(postEntities);
     } catch (e) {
       print('Erro na use case: $e');
-      rethrow;
+      return Left(GenericFailure(message: e.toString()));
     }
   }
 }
--- lib/modules/post/ui/stores/bloc/post_bloc.dart
+++ lib/modules/post/ui/stores/bloc/post_bloc.dart
@@ -1,5 +1,3 @@
-import 'package:case_fire/modules/post/domain/entities/post_entity.dart';
-import 'package:case_fire/modules/post/domain/repositories/post_repository.dart';
 import 'package:case_fire/modules/post/domain/usecases/get_posts_usecase.dart';
 import 'package:case_fire/modules/post/ui/stores/bloc/post_event.dart';
 import 'package:case_fire/modules/post/ui/stores/bloc/post_state.dart';
@@ -8,19 +6,18 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class PostBloc extends Bloc<PostEvent, PostState> {
   final GetPostsUseCase _getPostsUseCase;
 
-  PostBloc(PostRepository postRepository)
-    : _getPostsUseCase = GetPostsUseCase(postRepository),
+  PostBloc({required GetPostsUseCase getPostsUseCase})
+    : _getPostsUseCase = getPostsUseCase,
       super(PostInitial()) {
     on<GetPosts>(_getPostsToState);
   }
 
   Future<void> _getPostsToState(GetPosts event, Emitter<PostState> emit) async {
     emit(PostLoading());
-    try {
-      final List<PostEntity> posts = await _getPostsUseCase.execute();
-      emit(PostLoaded(posts));
-    } catch (e) {
-      emit(PostError(e.toString()));
-    }
+    final result = await _getPostsUseCase.execute();
+    result.fold(
+      (failure) => emit(PostError(failure.toString())),
+      (posts) => emit(PostLoaded(posts)),
+    );
   }
 }
