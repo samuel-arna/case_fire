7ee2c2eaab36d84a76456d9480503e4315ecc7e2
Samuel Arná <samuka.arna@gmail.com>
2025-04-08 06:18:32 -0300

    fix: remove author info into post model and entity

--- lib/modules/post/domain/entities/post_entity.dart
+++ lib/modules/post/domain/entities/post_entity.dart
@@ -1,39 +1,24 @@
 import 'package:case_fire/modules/post/data/models/post_model.dart';
 
 class PostEntity {
+  final int userId;
   final int id;
   final String title;
   final String body;
-  final int userId;
-  final String? userPhotoUrl;
-  final String? userName;
-  final int? userAge;
-  final List<String>? userGostos;
-  final int? numPost;
 
   PostEntity({
+    required this.userId,
     required this.id,
     required this.title,
     required this.body,
-    required this.userId,
-    this.userPhotoUrl,
-    this.userName,
-    this.userAge,
-    this.userGostos,
-    this.numPost,
   });
 
   factory PostEntity.fromModel(PostModel model) {
     return PostEntity(
-      id: model.id,
-      title: model.title,
-      body: model.body,
-      userId: model.userId,
-      userPhotoUrl: model.userPhotoUrl,
-      userName: model.userName,
-      userAge: model.userAge,
-      userGostos: model.userGostos,
-      numPost: model.numPost,
+      userId: model.userId ?? 0,
+      id: model.id ?? 0,
+      title: model.title ?? '',
+      body: model.body ?? '',
     );
   }
 }
