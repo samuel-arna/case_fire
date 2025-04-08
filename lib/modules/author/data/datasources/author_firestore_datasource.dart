94b778b4a1ae63aae77073581c5cfcb3c33cd5ad
Samuel Arná <samuka.arna@gmail.com>
2025-04-08 08:21:28 -0300

    feat: author datasource

---
+++ lib/modules/author/data/datasources/author_firestore_datasource.dart
@@ -0,0 +1,18 @@
+import 'package:case_fire/modules/author/data/models/author_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AuthorFirestoreDatasource {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<AuthorModel?> getAuthorInfo(String userId) async {
+    try {
+      final DocumentSnapshot<Map<String, dynamic>> snapshot =
+          await _firestore.collection('users').doc(userId).get();
+      final data = snapshot.data();
+      return data != null ? AuthorModel.fromJson(data) : null;
+    } catch (e) {
+      print('Erro ao buscar dados do autor no Firestore: $e');
+      return null;
+    }
+  }
+}
