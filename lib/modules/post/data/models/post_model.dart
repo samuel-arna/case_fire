7f526140524d9507a2e4072dd6ca61516d406bf6
Samuel Arná <samuka.arna@gmail.com>
2025-04-08 05:06:55 -0300

    fix: include author info

--- lib/modules/post/data/models/post_model.dart
+++ lib/modules/post/data/models/post_model.dart
@@ -1,24 +1,37 @@
 class PostModel {
-  int? userId;
-  int? id;
-  String? title;
-  String? body;
+  final int id;
+  final String title;
+  final String body;
+  final int userId;
+  final String? userPhotoUrl;
+  final String? userName;
+  final int? userAge;
+  final List<String>? userGostos;
+  final int? numPost;
 
-  PostModel({this.userId, this.id, this.title, this.body});
+  PostModel({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.userId,
+    this.userPhotoUrl,
+    this.userName,
+    this.userAge,
+    this.userGostos,
+    this.numPost,
+  });
 
-  PostModel.fromJson(Map<String, dynamic> json) {
-    userId = json['userId'];
-    id = json['id'];
-    title = json['title'];
-    body = json['body'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['userId'] = userId;
-    data['id'] = id;
-    data['title'] = title;
-    data['body'] = body;
-    return data;
+  factory PostModel.fromJson(Map<String, dynamic> json) {
+    return PostModel(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      body: json['body'] as String,
+      userId: json['userId'] as int,
+      userPhotoUrl: json['foto'] as String?,
+      userName: json['nome'] as String?,
+      userAge: json['idade'] as int?,
+      userGostos: (json['gostos'] as List<dynamic>?)?.cast<String>(),
+      numPost: json['num_post'] as int?,
+    );
   }
 }
